fa2de561b94f57a515eb77d9ad516148f202fb61
kalpshah01 <kalpshah962005@gmail.com>
2026-01-16 20:14:29 +0530

    Add files via upload

---
+++ js/js/index.js
@@ -0,0 +1,34 @@
+
+let btn=document.getElementById('btn');
+btn.addEventListener("click",function(e){
+    e.preventDefault();
+    let name =document.getElementById('title').value;
+    let price=document.getElementById('price').value;
+    let img=document.getElementById('img').value;
+    let category=document.getElementById('category').value;
+    console.log(name,price,img,category);
+    if(name=="" || price==""||img==""||category==""){
+        alert("please enter data properly");
+        return;
+    }
+    if(price<0){
+        alert("price must be grater than 0");
+    }
+    else{
+        let users=JSON.parse(localStorage.getItem("users"))||[];
+
+        let obj={
+            id:Date.now(),
+            name,
+            price,
+            img,
+            category
+        }
+        console.log(obj);
+            users.push(obj);
+            console.log(" pushing dat");
+            console.log(users);
+        localStorage.setItem("users",JSON.stringify(users));
+        console.log(" data added");
+    }
+});
